bfb59c14c98a5192b580d3f6586703e3bd2ea5d6
kavinKSH <kavin14795@gmail.com>
2023-11-21 19:00:24 +0530

    complete chat vc

---
+++ Practice gitApp/ChatViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ChatViewController.swift
+//  Practice gitApp
+//
+//  Created by Kavin on 21/11/23.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Chat VC Complete")
+    }
+    
+}
