f1cbdfba8bb57b471baf6301eaff9e13f12b6ccc
kavinKSH <kavin14795@gmail.com>
2023-11-21 18:57:04 +0530

    complete loginVC

---
+++ Practice gitApp/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  Practice gitApp
+//
+//  Created by Kavin on 21/11/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("LoginVC Complete")
+    }
+}
