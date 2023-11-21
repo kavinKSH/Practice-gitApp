6f0c7d9ba0aba8e342eae5708980bf8a6fe6e785
kavinKSH <kavin14795@gmail.com>
2023-11-21 18:53:37 +0530

    complete registerVC

---
+++ Practice gitApp/RegisterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterViewController.swift
+//  Practice gitApp
+//
+//  Created by Kavin on 21/11/23.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Complete the RegisterVC")
+    }
+}
