f31a487e1b66c082e3b277f0660be07804513372
kavinKSH <kavin14795@gmail.com>
2023-11-21 19:04:03 +0530

    complete list vc

---
+++ Practice gitApp/ListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewController.swift
+//  Practice gitApp
+//
+//  Created by Kavin on 21/11/23.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("List VC Complete")
+       
+    }
+}
