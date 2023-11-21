1ea1ceee0eac98b1c9851bf3804c9202d86dbc03
kavinKSH <kavin14795@gmail.com>
2023-11-21 19:11:05 +0530

    complete table VC

---
+++ Practice gitApp/TableViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewController.swift
+//  Practice gitApp
+//
+//  Created by Kavin on 21/11/23.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Table VC Complete")
+
+    }
+}
+
+
